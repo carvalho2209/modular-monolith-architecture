146a428f3be325e26a5719d5bb8d460e7e92de83
Alexandre Carvalho <carvalho2209@hotmail.com>
2024-08-19 15:19:13 -0700

    Add Architecture Testing

---
+++ test/Evently.ArchitectureTests/Abstractions/BaseTest.cs
@@ -0,0 +1,18 @@
+﻿namespace Evently.ArchitectureTests.Abstractions;
+
+#pragma warning disable CA1515
+public abstract class BaseTest
+#pragma warning restore CA1515
+{
+    protected const string UsersNamespace = "Evently.Modules.Users";
+    protected const string UsersIntegrationEventsNamespace = "Evently.Modules.Users.IntegrationEvents";
+
+    protected const string EventsNamespace = "Evently.Modules.Events";
+    protected const string EventsIntegrationEventsNamespace = "Evently.Modules.Events.IntegrationEvents";
+
+    protected const string TicketingNamespace = "Evently.Modules.Ticketing";
+    protected const string TicketingIntegrationEventsNamespace = "Evently.Modules.Ticketing.IntegrationEvents";
+
+    protected const string AttendanceNamespace = "Evently.Modules.Attendance";
+    protected const string AttendanceIntegrationEventsNamespace = "Evently.Modules.Attendance.IntegrationEvents";
+}
